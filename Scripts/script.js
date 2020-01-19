649708b54d14fa05a0f4839a307cd31325311ff6
Yash Sharma <mr.sharmayash@outlook.com>
2020-01-18 16:14:06 -0800

    joinEvent information displayed

--- Scripts/script.js
+++ Scripts/script.js
@@ -136,12 +136,10 @@ const db = firebase.firestore();
           })
           thisMarker.addListener('click', function(e){
             if(doc.data().hosted){
-              console.log("bitch hosted");
-              infoWindow.setContent('<h1>' + doc.data().title + '</h1>'+'<img src='+doc.data().Img+'height = 40% width=40%'+' >'+'<a href="Pages/host.html" class="btn btn-light">Host the Event</a>');
+              infoWindow.setContent('<h1>' + doc.data().title + '</h1>'+'<img src='+doc.data().Img+'height = 40% width=40%'+' >'+'<a href="Pages/host.html?' + doc.id + '"  class="btn btn-light">Host the Event</a>');
             }
             else if(!doc.data().hosted){
-              console.log("join bitch?");
-              infoWindow.setContent('<h1>' + doc.data().title + '</h1>'+'<img src='+doc.data().Img+'height = 40% width=40%'+' >'+'<a href="Pages/join.html" class="btn btn-light">Join the Event</a>')
+              infoWindow.setContent('<h1>' + doc.data().title + '</h1>'+'<img src='+doc.data().Img+'height = 40% width=40%'+' >'+'<a href="Pages/join.html?' + doc.id + '" class="btn btn-light">Join the Event</a>')
             }           
             infoWindow.open(map, thisMarker);onclick
           })
